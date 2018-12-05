fad4c42ae4f331c7867f001361e5daaafc1538c4
ashlooper <44914149+ashlooper@users.noreply.github.com>
2018-12-05 16:07:44 +0800

    Update LinkedListDeque.java

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -31,7 +31,7 @@ public class LinkedListDeque<T> {
 	}
 */
 	public boolean isEmpty() {
-		if(size == 0) {
+		if (size == 0) {
 			return true;
 		}
 		return false;
@@ -55,7 +55,7 @@ public class LinkedListDeque<T> {
 
 	public void printDeque() {
 		Node temp = sentinel;
-		while(temp.next != sentinel) {
+		while (temp.next != sentinel) {
 			temp = temp.next;
 			System.out.print(temp.item);
 			System.out.print(" ");
@@ -64,31 +64,33 @@ public class LinkedListDeque<T> {
 	}
 
 	public T removeFirst() {
-		if(size == 0){
+		if (size == 0){
 			return null;
 		}
+		T removeItem = sentinel.next.item;
 		sentinel.next = sentinel.next.next;
 		sentinel.next.prev = sentinel;
 		size -= 1;
-		return sentinel.next.item;
+		return removeItem;
 	}
 
 	public T removeLast() {
-		if(size == 0) {
+		if (size == 0) {
 			return null;
 		}
+		T removeItem = sentinel.prev.item;
 		sentinel.prev = sentinel.prev.prev;
 		sentinel.prev.next = sentinel;
 		size -= 1;
-		return sentinel.prev.item;
+		return removeItem;
 	}
 
 	public T get(int index) {
 		Node temp = sentinel;
 		int count = 0;
-		while(temp.next != sentinel) {
+		while (temp.next != sentinel) {
 			temp = temp.next;
-			if(count == index) {
+			if (count == index) {
 				return temp.item;
 			}
 			count++;
@@ -97,14 +99,14 @@ public class LinkedListDeque<T> {
 	}
 
 	private T getRecursiveHelper(int index, Node temp, int count) {
-		if(index == count) {
+		if (index == count) {
 			return temp.item;
 		}
-		return getRecursiveHelper(index, temp.next, count+1);
+		return getRecursiveHelper(index, temp.next, count + 1);
 	}
 
 	public T getRecursive(int index) {
-		if(index >=size || index < 0){
+		if (index >=size || index < 0) {
 			return  null;
 		}
 		int count = 0;
